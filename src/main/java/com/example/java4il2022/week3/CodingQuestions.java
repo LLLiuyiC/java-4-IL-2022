21dd81e28c5f87731684b4b239ac78bf97a600eb
renqingyang <rq625230882@gmail.com>
2022-03-21 12:14:02 -0500

    Initial commit

---
+++ src/main/java/com/example/java4il2022/week3/CodingQuestions.java
@@ -0,0 +1,10 @@
+package com.example.java4il2022.week3;
+
+/**
+ * 1. two sum
+ * 2. verify palindrome
+ * 3. verify anagram
+ * 4. group anagram
+ * 5. validate parentheses , input "{}[]()" return true
+ * 6. find first unique character from input string
+ */
---
+++ src/test/java/com/example/java4il2022/Java4Il2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java4il2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java4Il2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
