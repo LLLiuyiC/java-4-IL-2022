31a035b60053eee7f82478bc6e75f124d90a126c
renqingyang <rq625230882@gmail.com>
2022-03-23 13:20:14 -0500

    Day12

--- src/main/java/com/example/java4il2022/week3/CodingQuestions.java
+++ src/main/java/com/example/java4il2022/week3/CodingQuestions.java
@@ -7,4 +7,7 @@ package com.example.java4il2022.week3;
  * 4. group anagram
  * 5. validate parentheses , input "{}[]()" return true
  * 6. find first unique character from input string
+ * 7. Singleton class implementation (thread safe double check /lazy loading singleton)
+ * 8. java 8 stream api
+ *    given list of integer,  filter list , return sorted odd numbers
  */
